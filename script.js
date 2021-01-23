0bc153ba2449cd1a08365bfe146e6c7a2dad65ff
adrs <ra.condezomonge@gmail.com>
2021-01-23 01:15:05 -0500

    Fix(script.js) - Fix error message:vertical resize +5.

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function checkLength(input, min, max) {
 // Check passwords matche
 function checkPasswordsMatch(input1, input2) {
   if (input1.value !== input2.value) {
-    showError(input2, "Password do not match");
+    showError(input2, "* Password do not match");
   }
 }
 
