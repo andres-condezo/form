04e903ce8a670ce1027daa9cc3ed38fa1bb3641f
adrs <ra.condezomonge@gmail.com>
2021-01-23 01:01:09 -0500

    Feat(script.js) - Add length email validator and passwords match.

--- script.js
+++ script.js
@@ -19,10 +19,13 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-  ("]])");
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "* Email is not valid");
+  }
 }
 
 // check required fields
@@ -36,6 +39,30 @@ function checkRequired(inputArr) {
   });
 }
 
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `* ${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `* ${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Check passwords matche
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Password do not match");
+  }
+}
+
 // Get fieldname
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -46,4 +73,8 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password_confirmation]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 20);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
